2552f32aab1cad6093cc272f18b8d6c34e55f14f
MICHEL RODRIGUES DE OLIVEIRA <michel.roliveira@senacsp.edu.br>
2023-07-27 17:22:57 -0300

    Adicionado formulario de inclusão - Havaiana

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -7,6 +7,8 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        [BindProperty]
+        public Pizza Newpizza { get; set; } = default!;
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
@@ -18,6 +20,24 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid || Newpizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(Newpizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
         }
     }
 }
